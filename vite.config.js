50cf0868cdefc9fbab3073446830dc5098c0237e
Aqsa Abbasi <aqsaabbasi2690@gmail.com>
2025-03-29 09:38:14 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
-  base: 'https://github.com/Aqsaabbasi2690/practice1'
+  base: '/practice1'
 })
